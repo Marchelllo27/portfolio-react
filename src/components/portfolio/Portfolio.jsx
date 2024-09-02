9c66239439ce0079919a810a7406df8bc95e3a7c
Marc Tofan <tofmana@gmail.com>
2024-09-01 22:06:59 -0700

    project order changed and unique id added to each project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -17,12 +17,6 @@ const data = [
     demo: "https://clothes-e-commerce-shop-d7ly0a242-marchelllo27s-projects.vercel.app",
   },
   {
-    image: brainwave,
-    title: "Generative AI made for creators",
-    github: "https://github.com/Marchelllo27/brainwave-",
-    demo: "https://brainwave-ai-service.netlify.app",
-  },
-  {
     image: tShirt,
     title: "The 3D T-Shirt Customization",
     github: "https://github.com/Marchelllo27/threejs-tshirt",
@@ -35,35 +29,18 @@ const data = [
     demo: "https://youtube-clone-chi-virid.vercel.app",
   },
   {
+    image: brainwave,
+    title: "Generative AI made for creators",
+    github: "https://github.com/Marchelllo27/brainwave-",
+    demo: "https://brainwave-ai-service.netlify.app",
+  },
+  {
     image: eat,
     title: "The Virtual Restaurant(MERN Stack)",
     github: "https://github.com/Marchelllo27/final-project-passerelle-react",
     demo: "https://eat-smart-app.netlify.app",
   },
-  // {
-  //   image: church,
-  //   title: "The church website (HTML/CSS/JS)",
-  //   github: "https://github.com/Marchelllo27/Marchelllo27.github.io",
-  //   demo: "https://marchelllo27.github.io/",
-  // },
-  // {
-  //   image: hosting,
-  //   title: "The Hosting Website(HTML/CSS)",
-  //   github: "https://github.com/Marchelllo27/loruki-website",
-  //   demo: "https://loruki-demo-hosting.netlify.app",
-  // },
-  // {
-  //   image: travel,
-  //   title: "The travel website(HTML/CSS/JS)",
-  //   github: "https://github.com/Marchelllo27/Travel.CreatWebsite-With-Video-Background",
-  //   demo: "https://travel-backgroundvideo.netlify.app/",
-  // },
 ];
-
-// add unique Id to each element.
-data.forEach(item => {
-  item.id = item.title;
-});
 
 const Portfolio = () => {
   return (
@@ -73,9 +50,10 @@ const Portfolio = () => {
 
       <div className={`container ${classes["portfolio__container"]}`}>
         {data.map(project => (
-          <Project key={project.id} id={project.id} data={project} />
+          <Project key={project.title} data={project} />
         ))}
       </div>
+      
     </section>
   );
 };
