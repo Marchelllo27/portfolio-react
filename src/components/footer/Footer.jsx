d0533a0796032537b2c944b3dd90316f2be11b93
Marc Tofan <tofmana@gmail.com>
2024-09-16 18:28:10 -0700

    linkedin link fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 
 import { FaFacebookF } from "react-icons/fa";
 import { BsLinkedin } from "react-icons/bs";
@@ -7,34 +7,48 @@ import classes from "./Footer.module.css";
 import context from "../../store/context";
 
 const Footer = () => {
-  const navCtx = useContext(context)
+  const navCtx = useContext(context);
 
-  const onLinkClickHandler = (value) => {
+  const onLinkClickHandler = value => {
     navCtx.changeActiveLink(value);
   };
 
   return (
     <footer id="footer">
-      <a href="/#" className={classes["footer__logo"]}>Tofan Mark</a>
+      <a href="/#" className={classes["footer__logo"]}>
+        Tofan Mark
+      </a>
 
       <ul className={classes.permalinks}>
         <li>
-          <a href="/#" onClick={onLinkClickHandler.bind(null, "#")}>Home</a>
+          <a href="/#" onClick={onLinkClickHandler.bind(null, "#")}>
+            Home
+          </a>
         </li>
         <li>
-          <a href="#about" onClick={onLinkClickHandler.bind(null, "#about")}>About</a>
+          <a href="#about" onClick={onLinkClickHandler.bind(null, "#about")}>
+            About
+          </a>
         </li>
         <li>
-          <a href="#experience" onClick={onLinkClickHandler.bind(null, "#experience")}>Experience</a>
+          <a href="#experience" onClick={onLinkClickHandler.bind(null, "#experience")}>
+            Experience
+          </a>
         </li>
         <li>
-          <a href="#libraries" onClick={onLinkClickHandler.bind(null, "#services")}>Libraries</a>
+          <a href="#libraries" onClick={onLinkClickHandler.bind(null, "#services")}>
+            Libraries
+          </a>
         </li>
         <li>
-          <a href="#portfolio" onClick={onLinkClickHandler.bind(null, "#portfolio")}>Portfolio</a>
+          <a href="#portfolio" onClick={onLinkClickHandler.bind(null, "#portfolio")}>
+            Portfolio
+          </a>
         </li>
         <li>
-          <a href="#contact" onClick={onLinkClickHandler.bind(null, "#contact")}>Contact</a>
+          <a href="#contact" onClick={onLinkClickHandler.bind(null, "#contact")}>
+            Contact
+          </a>
         </li>
       </ul>
 
@@ -42,11 +56,10 @@ const Footer = () => {
         <a href="https://www.facebook.com/profile.php?id=100080298755222" target="_blank" rel="noreferrer">
           <FaFacebookF />
         </a>
-        <a href="https://www.linkedin.com/in/marktofan onClick={}" target="_blank" rel="noreferrer">
+        <a href="https://www.linkedin.com/in/marktofan" target="_blank" rel="noreferrer">
           <BsLinkedin />
         </a>
       </div>
-
 
       <div className={classes["footer__copyright"]}>
         <small>&copy; Tofan Mark. All rights reserved.</small>
